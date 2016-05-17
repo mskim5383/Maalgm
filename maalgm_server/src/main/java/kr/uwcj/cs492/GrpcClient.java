be49dccb015b15272de7a71e077b7fadbec83257
Park Junwoo <junwoopark0130@gmail.com>
2016-05-17 13:24:16 +0900

    simple gRPC server and client implemented. receives request number from outside server using URL.

---
+++ maalgm_server/src/main/java/kr/uwcj/cs492/GrpcClient.java
@@ -0,0 +1,88 @@
+package kr.uwcj.cs492;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+public class GrpcClient {
+  private static final Logger logger = Logger.getLogger(GrpcClient.class.getName());
+
+  private final ManagedChannel channel;
+  private final feedGrpc.feedBlockingStub blockingStub;
+
+  public GrpcClient(String host, int port) {
+    channel = ManagedChannelBuilder.forAddress(host, port)
+        .usePlaintext(true)
+        .build();
+    blockingStub = feedGrpc.newBlockingStub(channel);
+  }
+
+  public void shutdown() throws InterruptedException {
+    logger.info("Terminating client...");
+    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+  }
+
+  public void askMessage(int number) {
+    logger.info("Send message with the input number : " + number);
+    FeedServer.FeedRequest request = FeedServer.FeedRequest.newBuilder()
+        .setNumber(number)
+        .build();
+
+    FeedServer.FeedReply response;
+    try {
+      response = blockingStub.test(request);
+    } catch (StatusRuntimeException e) {
+      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+      return;
+    }
+
+    if(response.getMessage().equals("True")) {
+      logger.info("The return value with input number : " + number + " is True");
+    }
+    else {
+      logger.info("The return value with input number : " + number + " is False");
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    GrpcClient client = new GrpcClient("localhost", 21035);
+
+    while(true) {
+      Scanner reader = new Scanner(System.in);
+      //logger.info("Enter a integer(try \"exit\" to terminate program): ");
+      logger.info("Enter the URL to retrieve input.");
+      String url_input = reader.nextLine();
+      URL url = new URL(url_input);
+      BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+      String input = in.readLine();
+      logger.info("input read from the url: " + input);
+
+      if (!input.trim().equalsIgnoreCase("exit")) {
+        try {
+          int number = Integer.parseInt(input);
+          client.askMessage(number);
+        } catch (NumberFormatException e) {
+          logger.warning("Cannot parse input as integer");
+        }
+      }
+      else {
+        try {
+          client.shutdown();
+          break;
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
---
+++ maalgm_server/src/main/java/kr/uwcj/cs492/GrpcServer.java
@@ -0,0 +1,73 @@
+package kr.uwcj.cs492;
+
+
+import com.google.api.client.repackaged.com.google.common.annotations.VisibleForTesting;
+
+import io.grpc.stub.StreamObserver;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class GrpcServer {
+
+  private static final Logger logger = Logger.getLogger(GrpcServer.class.getName());
+
+  private Server server;
+  private static final int port = 21035;
+
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final GrpcServer server = new GrpcServer();
+    server.start();
+    server.blockUntilShutdown();
+  }
+
+  @VisibleForTesting
+  void start() throws IOException {
+    server = ServerBuilder.forPort(port)
+        .addService(feedGrpc.bindService(new FeedImpl()))
+        .build()
+        .start();
+    logger.info("Server started, listening on port : " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        System.err.println("*** shutting down gRPC server since JVM is shutting down.");
+        GrpcServer.this.stop();
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  private void stop() {
+    if (server != null) {
+      server.shutdown();
+    }
+  }
+
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+  @VisibleForTesting
+  static class FeedImpl implements feedGrpc.feed {
+
+    @Override
+    public void test(FeedServer.FeedRequest request, StreamObserver<FeedServer.FeedReply> responseObserver) {
+      FeedServer.FeedReply reply = FeedServer.FeedReply.newBuilder()
+          .setMessage(produceMessage(request.getNumber()))
+          .build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
+    }
+
+    private String produceMessage(long number) {
+      return "Return Message";
+    }
+
+  }
+}
