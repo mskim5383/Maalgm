463678c805186c38418edee23bc10f9eefb40a63
Jaemin Shin <999fg@kaist.ac.kr>
2016-06-03 20:44:29 +0900

    Basic db insertion code for signup implemented.

--- MongoDB/src/main/java/Maalgm/MDBJavaServer.java
+++ MongoDB/src/main/java/Maalgm/MDBJavaServer.java
@@ -1,12 +1,16 @@
 package Maalgm;
 
 import org.bson.Document;
+import com.mongodb.Block;
+import com.mongodb.client.FindIterable;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Sorts.ascending;
 import static java.util.Arrays.asList;
 
 import com.mongodb.MongoClient;
@@ -15,32 +19,20 @@ import com.mongodb.client.MongoDatabase;
 public class MDBJavaServer {
     public static void main(String[] args) {
       MongoClient mongoClient = new MongoClient();
-      MongoDatabase db = mongoClient.getDatabase("test");
-
-      DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
-      try{
-        db.getCollection("restaurants").insertOne(
-        new Document("address",
-                new Document()
-                        .append("street", "2 Avenue")
-                        .append("zipcode", "10075")
-                        .append("building", "1480")
-                        .append("coord", asList(-73.9557413, 40.7720266)))
-                .append("borough", "Manhattan")
-                .append("cuisine", "Italian")
-                .append("grades", asList(
-                        new Document()
-                                .append("date", format.parse("2014-10-01T00:00:00Z"))
-                                .append("grade", "A")
-                                .append("score", 11),
-                        new Document()
-                                .append("date", format.parse("2014-01-16T00:00:00Z"))
-                                .append("grade", "B")
-                                .append("score", 17)))
-                .append("name", "Vella")
-                .append("restaurant_id", "41704620")); 
-      } catch (ParseException e) {
-        e.printStackTrace();
-      }
+      MongoDatabase db = mongoClient.getDatabase("grpcdb");
+      db.getCollection("peruser").insertOne(
+          new Document()
+              .append("username", "username001")
+              .append("passwd", "1234567890")
+              .append("realname", "John Smith")
+              .append("urllist", asList())
+              .append("urlcount", 0));
+      FindIterable<Document> iterable = db.getCollection("peruser").find();
+      iterable.forEach(new Block<Document>() {
+        @Override
+        public void apply(final Document document) {
+          System.out.println(document);
+        }
+      });
     }    
 }
